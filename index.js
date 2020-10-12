0f4901b806b72c2801026fb2ac8f7a8384d90286
wilyanto <wilyanto24@gmail.com>
2020-10-12 12:33:38 +0700

    insert photo using fs

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 import express from 'express'
 import hbs from 'hbs'
 import path from 'path'
@@ -5,6 +8,8 @@ import morgan from 'morgan'
 import bodyParseer from 'body-parser'
 import { initDatabase, initTable, insertProduct, getProduct} from './database.js'
 import { get } from 'http'
+import fileUpload from 'express-fileupload'
+import fs from 'fs'
 
 const __dirname = path.resolve()
 
@@ -17,6 +22,9 @@ app.set('views', __dirname + '/layouts')
 app.set('view engine', 'html')
 app.engine('html', hbs.__express)
 
+// use file parser
+app.use(fileUpload())
+
 // log incoming request
 app.use(morgan('combined'))
 
@@ -24,7 +32,8 @@ app.use(morgan('combined'))
 app.use(bodyParseer.urlencoded())
 
 // serve static file
-app.use('assets/', express.static(__dirname + 'assets'))
+app.use('/assets', express.static(__dirname + '/assets'))
+app.use('/files', express.static(__dirname + '/files'))
 
 app.get('/', (req, res, next) => {
     res.send({success: true})
@@ -58,19 +67,31 @@ app.get('/add-product', (req, res, next) => {
 
 app.post('/add-product', (req, res, next) => {
     console.log('Request', req.body)
+    console.log('File', req.files)
 
-    // insert product
-    insertProduct(db, req.body.name, parseInt(req.body.price), '-')
+    // get file name
+    const fileName = Date.now() + req.files.photo.name
 
-    // redirect
-    res.redirect('/product')
-    res.send(req.body)
+    // write file
+    fs.writeFile(path.join(__dirname, '/files', fileName), req.files.photo.data, (err) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+
+        // insert product
+        insertProduct(db, req.body.name, parseInt(req.body.price), `/files/${fileName}`)
+
+        // redirect
+        res.redirect('/product')
+        // res.send(req.body)
+    })
 })
 
 app.use((err, req, res, next) => {
     res.send(err.message)
 })
 
-app.listen(8000, () => {
-    console.log('App listen on port 8000')
+app.listen(process.env.PORT, () => {
+    console.log('App listen on port ', process.env.PORT)
 })
